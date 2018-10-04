466e3fc5b72f7572f03f18f875ff91dc1d08a879
Manuel Sanchez <manuel.sanchez@ciberexperis.es>
2018-10-04 17:08:06 +0200

    Subida de proyecto funcional con todos sus puntos.

---
+++ src/main/java/com/rest/controllers/InicioSessionDateController.java
@@ -0,0 +1,111 @@
+package com.rest.controllers;
+
+import java.util.List;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.rest.config.ApplicationConfig;
+import com.rest.entity.Contact;
+import com.rest.services.GenerPasswdService;
+import com.rest.services.impl.ContactServiceImpl;
+
+@org.springframework.web.bind.annotation.RestController
+@RequestMapping("/apc")
+public class InicioSessionDateController {
+
+	@Autowired
+	@Qualifier("contactServiceImpl")
+	private ContactServiceImpl contactServiceImpl;
+
+	// RESTABLECER CONTRASEÑA Y ENVIAR POR EMAIL
+	@PutMapping("/person/resetPasswd")
+	public ResponseEntity<?> SendMail(@RequestBody Contact contact, String To, String Subject, String Mensage) {
+
+		Session session = Session.getInstance(ApplicationConfig.getMailSender(), new javax.mail.Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(contact.getEmail(), contact.getPasswd());
+			}
+		});
+		try {
+			String passwd = GenerPasswdService.getPassword(
+					GenerPasswdService.MINUSCULAS + GenerPasswdService.MAYUSCULAS + GenerPasswdService.ESPECIALES, 10);
+
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress(contact.getEmail()));
+			message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(contact.getEmail()));// email->receptivo
+			message.setSubject("Correo de generación de una nueva contraseña");
+			message.setText("Tu nueva contraseña será: " + passwd);
+			Transport.send(message);
+			if (!contact.getEmail().trim().isEmpty() && !contact.getPasswd().trim().isEmpty()) {
+				List<Contact> contactos = contactServiceImpl.findByNombreOrderById(contact.getNombre());
+
+				for (Contact user : contactos) {
+					if (contact.getEmail().equals(user.getEmail()) && contact.getPasswd().length() > 8) {
+
+						contactServiceImpl.updatePasswd(user, passwd);
+
+					}
+				}
+			}
+			return ResponseEntity.ok(
+					HttpStatus.OK + " Se envió un correo con el cambio de contraseña al mail: " + contact.getEmail());
+
+		} catch (MessagingException e) {
+			throw new RuntimeException(e);// Si se produce un error
+		}
+	}
+
+	// CAMBIAR EMAIL
+	@PutMapping("/person/emailEdit")
+	public String updateEmail(@RequestBody Contact contact) {
+		if (!contact.getEmail().trim().isEmpty() && !contact.getPasswd().trim().isEmpty()) {
+			List<Contact> contactos = contactServiceImpl.findByNombreOrderById(contact.getNombre());
+
+			for (Contact user : contactos) {
+				if (contact.getPasswd().equals(user.getPasswd())) {
+
+					contactServiceImpl.updateEmail(user, contact.getEmail());
+
+					return ResponseEntity.ok(HttpStatus.OK
+							+ " Se ha modificado el email de manera manual con nombre actual: " + contact.getEmail())
+							.toString();
+				}
+			}
+		}
+		return ResponseEntity.ok(HttpStatus.UNAUTHORIZED + " Error de cambio de email del usuario").toString();
+	}
+
+	// CAMBIAR CONTRASEÑA
+	@PutMapping("/person/passwdEdit")
+	public String updatePasswd(@RequestBody Contact contact) {
+		if (!contact.getEmail().trim().isEmpty() && !contact.getPasswd().trim().isEmpty()) {
+			List<Contact> contactos = contactServiceImpl.findByNombreOrderById(contact.getNombre());
+
+			for (Contact user : contactos) {
+				if (contact.getEmail().equals(user.getEmail()) && contact.getPasswd().length() > 8) {
+
+					contactServiceImpl.updatePasswd(user, contact.getPasswd());
+
+					return ResponseEntity.ok(HttpStatus.OK
+							+ " Se ha modificado la contraseña de manera manual del email: " + contact.getEmail())
+							.toString();
+				}
+			}
+		}
+		return ResponseEntity.ok(HttpStatus.UNAUTHORIZED + " Error de cambio contraseña del usuario").toString();
+	}
+}
---
+++ src/main/java/com/rest/services/impl/ContactServiceImpl.java
@@ -0,0 +1,68 @@
+package com.rest.services.impl;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import com.rest.entity.Contact;
+import com.rest.repositories.LoginRepository;
+import com.rest.services.ContactService;
+
+@Service("contactServiceImpl")
+public class ContactServiceImpl implements ContactService {
+
+	@Autowired
+	@Qualifier("loginRepository")
+	private LoginRepository loginRepository;
+
+	@Override
+	public List<Contact> findByNombreOrderById(String nombre) {
+		return loginRepository.findAll();
+	}
+
+	@Override
+	public Contact addContact(Contact contact) {
+		return loginRepository.save(contact);
+
+	}
+
+	@Override
+	public void removeContact(Contact contact) {
+		loginRepository.delete(contact);
+	}
+
+	@Override
+	public String updateContact(Contact contact, String nombre, String appellidos, int edad, String localidad) {
+
+		contact.setNombre(nombre);
+		contact.setAppellidos(appellidos);
+		contact.setEdad(edad);
+		contact.setLocalidad(localidad);
+
+		loginRepository.save(contact);
+		return " ";
+	}
+
+	@Override
+	public Optional<Contact> findById(int id) {
+		return loginRepository.findById(new Integer(id));
+	}
+
+	@Override
+	public String updatePasswd(Contact contact, String passwd) {
+		contact.setPasswd(passwd);
+		loginRepository.save(contact);
+		return null;
+	}
+
+	@Override
+	public String updateEmail(Contact contact, String email) {
+		contact.setEmail(email);
+		loginRepository.save(contact);
+		return null;
+	}
+
+}
