4ff580c6ea8f6fb034d4b6d3ea85ee4029a6fac2
Manuel Sanchez <manuel.sanchez@ciberexperis.es>
2018-10-11 14:06:31 +0200

    Subida en new repository

--- src/main/java/com/rest/MicroservicesApplication.java
+++ src/main/java/com/rest/MicroservicesApplication.java
@@ -6,7 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MicroservicesApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		SpringApplication.run(MicroservicesApplication.class, args);
 	}
+
 }
